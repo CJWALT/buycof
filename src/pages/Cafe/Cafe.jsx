1616353fc33ad40087cf43258075df117bf13a11
cjwalt <oyedelee22@gmail.com>
2024-04-24 05:08:38 -0700

    update #181: add more width to cafe-wrap, search and sort wrap

--- src/pages/Cafe/Cafe.jsx
+++ src/pages/Cafe/Cafe.jsx
@@ -31,19 +31,19 @@ const Cafe = ()=>{
                     <div className='cafe-wrap' key={item.id}>
                         <div className='cafe-img__wrap'>
                             <div className='cafe-img_small'> 
-                            <img src={item.image_url} alt= ""/>
-                        </div>
+                                <img src={item.image_url} alt= ""/>
+                            </div>
             
-                        <div className='cafe-img_large'>
-                            <img src={item.image_url} alt=""/>
-                        </div>
+                            <div className='cafe-img_large'>
+                                <img src={item.image_url} alt=""/>
+                            </div>
                         </div>
                   
         
                     <div className='cafe-details'>
                         <div>
                             <h4 className='cafe-name'>{item.name}</h4>
-                            <small className='cafe-price'>{item.price}</small>
+                            <small className='cafe-price'>${item.price}</small>
                         </div>
         
                         <p className='cafe-flavor'>{item.flavor_profile}</p>
